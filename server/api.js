079d2bb6f904c944daef5df1e704ab716bc0fdfc
Sinan Bolel <sinanbolel@gmail.com>
2017-05-10 17:21:36 -0500

    refactor: server index, api

--- server/api.js
+++ server/api.js
@@ -1,19 +1,19 @@
-const Router = require('express').Router
+const api = require('express').Router() // eslint-disable-line new-cap
 const Renderer = require('./renderer')
 
-const api = new Router()
-
+// set global CORS
 api.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
 
+// index route
 api.get('/', (req, res) => {
   const render = new Renderer('app/views/index')
   render()
     .then(html => res.send(html))
-    .catch(err => res.send({status: 500, message: JSON.stringify(err)}))
+    .catch(err => res.send({status: 500, message: err}))
 })
 
 module.exports = api
