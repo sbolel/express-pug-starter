dac9f85130ba556508c679a5b3a43faebdc3e00e
Sinan Bolel <sinanbolel@gmail.com>
2017-05-10 18:46:42 -0500

    chore(gulp): switch to 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,39 @@
 const gulp = require('gulp')
+const path = require('path')
 const sass = require('gulp-sass')
+const fs = require('fs')
+const nodemon = require('gulp-nodemon')
 
-gulp.task('sass', function () {
+function setup (done) {
+  const p = new Promise((resolve, reject) => fs.access('.env', fs.F_OK, err =>
+    err ? copyFile('.env_sample', '.env', resolve) : resolve()))
+  p.then(() => done()).catch(err => done(err))
+}
+
+
+function serve() {
+  nodemon({
+    ext: 'env js pug scss',
+    script: 'server/index.js',
+    tasks: changedFiles => {
+      const tasks = []
+      changedFiles.forEach(file => {
+        if (path.extname(file) === '.scss' && !~tasks.indexOf('styles')) {
+          tasks.push('styles')
+        }
+      })
+      return tasks
+    }
+  })
+}
+
+function styles () {
+  console.log('Rebuilding SASS...')
   return gulp.src('./app/styles/index.scss')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(gulp.dest('./app/dist'))
-})
+}
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./app/**/*.scss', ['sass'])
-})
+gulp.task('styles', gulp.series(styles))
 
-gulp.task('default', ['sass'])
+gulp.task('default', gulp.series(gulp.parallel(setup, styles), serve))
